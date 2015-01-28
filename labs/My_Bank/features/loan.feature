24fbe9d449edd96cbd644b0cd39ac1d81392c5e6
CoderWannaBe614 <radclim@yahoo.com>
2015-01-28 16:55:22 -0500

    My bank gherkins and pseudo code

---
+++ labs/My_Bank/features/loan.feature
@@ -0,0 +1,19 @@
+Feature: Loan Selection
+  As a user I want to view online loan information
+  So that I can pick a bank that has the best deal for me
+
+Scenario: New client views new loan information
+  When I select a new loan informaion
+  Then the loan information displays
+
+Scenario: Existing client views new loan information
+  When an existing customer selects a new loan type
+  Then new loan information displays with any existing loan information
+
+Scenario: Calculate new loan amortization
+  When I select the calculate option
+  Then the amortization information displays
+
+
+
+
---
+++ labs/My_Bank/features/step_definitions/my_bank_steps.rb
@@ -0,0 +1,43 @@
+When(/^I select a new loan informaion$/) do
+  #go to selection page
+
+  #select loan type
+
+  #select button to compile
+end
+
+Then(/^the (.*) displays$/) do |loan_rate|
+  #verify loan type displays
+
+  #verify loan rate displays
+
+  #Verify loan term displays
+
+  #verify offering bank displays
+
+end
+
+When(/^an existing customer selects a new loan type$/) do
+  #go to selection page
+
+  #select loan type
+
+  #select button to compile
+end
+
+Then(/^new loan information displays with any existing loan information$/) do
+  #verify loan type displays
+
+  #verify loan rate displays
+
+  #Verify loan term displays
+
+  #verify offering bank displays
+
+  #verify any existing loan information displays
+end
+
+When(/^I select the calculate option$/) do
+  #verify loan information displays in report
+end
+
